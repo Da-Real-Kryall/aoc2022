abb6a87a457835a8fc44d3a8bc920f2eb4f9163d
Da-Real-Kryall <codyryall05@gmail.com>
2022-12-03 14:24:56 +0800

    initialised days 4 through 10

---
+++ src/solutions/day06.rs
@@ -0,0 +1,13 @@
+// Day 6 solution by Da-Real-Kryall
+
+pub fn part1() -> i128 {
+    let input = include_str!("../inputs/day06.txt");
+    let mut current_total = 0;
+    current_total
+}
+
+pub fn part2() -> i128 {
+    let input = include_str!("../inputs/day06.txt");
+    let mut current_total = 0;
+    current_total
+}
